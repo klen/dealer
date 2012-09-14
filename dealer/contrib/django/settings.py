7b8b7f94e2306e406bc91e348ccd5a94cb87cc3d
Kirill Klenov <horneds@gmail.com>
2012-09-14 13:27:35 +0400

    fix django

--- dealer/contrib/django/settings.py
+++ dealer/contrib/django/settings.py
@@ -3,9 +3,9 @@ from dealer import get_backend
 
 
 TYPE = getattr(settings, 'DEALER_TYPE', 'auto')
-PATH = getattr(settings, 'DEALER_PATH')
-FILENAME = getattr(settings, 'DEALER_FILENAME')
-BACKENDS = getattr(settings, 'DEALER_BACKENDS')
+PATH = getattr(settings, 'DEALER_PATH', None)
+FILENAME = getattr(settings, 'DEALER_FILENAME', None)
+BACKENDS = getattr(settings, 'DEALER_BACKENDS', None)
 
 
 BACKEND = get_backend(TYPE, path=PATH, filename=FILENAME, backends=BACKENDS)
