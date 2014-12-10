ba40364e8e1bc7f3c1ed164005d8591e1894d740
Kirill Klenov <horneds@gmail.com>
2014-12-10 16:26:27 +0300

    Bump version: 1.1.1 → 2.0.0

--- dealer/__init__.py
+++ dealer/__init__.py
@@ -10,7 +10,7 @@ Use it for control a static's versions.
 
 """
 
-__version__ = '1.1.1'
+__version__ = '2.0.0'
 __project__ = __name__
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "BSD"
